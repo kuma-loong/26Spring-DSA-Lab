009d7d2356d93c712da3057c53ed6505b5c2eb22
kumaGu <gqs060905@outlook.com>
2026-04-23 10:48:21 +0800

    feat: finish lab BuildTree

--- Lab2/BuildTree.c
+++ Lab2/BuildTree.c
@@ -1,18 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SIZE 100
+
 struct TreeNode {
     int val;
     struct TreeNode *left;
     struct TreeNode *right;
 };
 
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
 struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize) {
-    // TODO
+    if (preorderSize == 0 || inorderSize == 0) {
+        return NULL;
+    }
+    struct TreeNode* root = createNode(preorder[0]);
+    for (int i = 0; i < inorderSize; i++) {
+        if (inorder[i] == preorder[0]) {
+            root->left = buildTree(preorder + 1, i, inorder, i);
+            root->right = buildTree(preorder + 1 + i, preorderSize - 1 - i, inorder + i + 1, inorderSize - 1 - i);
+            break;
+        }
+    }
+    return root;
+    
+
+
 }
 
 void printTree(struct TreeNode* root) {
-    // TODO
+    if (root == NULL) {
+        printf("null ");
+        return;
+    }
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(MAX_SIZE * sizeof(struct TreeNode*));
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    while (front != rear) {
+        struct TreeNode* current = queue[front++];
+        if (current == NULL) {
+            printf("null ");
+        } else {
+            printf("%d ", current->val);
+            queue[(rear++)%MAX_SIZE] = current->left;
+            queue[(rear++)%MAX_SIZE] = current->right;
+        }
+    }
 }
 
 int main() {
